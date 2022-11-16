df254453340ce086d6ecc1226f70236affd8df70
github_Ryounawan <rosarianto76@gmail.com>
2022-11-16 22:21:55 +0700

    GD

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  //tambahkan firebaseConfig project
+};
+
+//tambahkan code untuk menginisialisasikan Firebase
+const app = initializeApp(firebaseConfig);
+//tambahkan code untuk menginisialisasikan Realtime Database
+//dan mendapatkan referensi ke database tersebut
+export const db = getDatabase(firebaseApp);
