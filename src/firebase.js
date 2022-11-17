ef2dfa3a7a4234452df89906f6b7f4b70b225564
github_Ryounawan <rosarianto76@gmail.com>
2022-11-17 10:44:04 +0700

    UGD

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,24 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   //tambahkan firebaseConfig project
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAF-VWBL5aUbTIYzFtQaDko8071eduGbeI",
+  authDomain: "vuefire0813.firebaseapp.com",
+  databaseURL: "https://vuefire0813-default-rtdb.firebaseio.com",
+  projectId: "vuefire0813",
+  storageBucket: "vuefire0813.appspot.com",
+  messagingSenderId: "101517756969",
+  appId: "1:101517756969:web:2a93766ae293ed9b9b2a38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 };
 
 //tambahkan code untuk menginisialisasikan Firebase
